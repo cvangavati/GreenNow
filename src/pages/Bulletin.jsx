d71ace274c8c183a896a9f6f5ffb1c76eeb8e1cb
Charith Vangavati <cvangavati@gmail.com>
2026-07-16 20:55:22 -0400

    Fix missing default export in Bulletin.jsx

---
+++ src/pages/Bulletin.jsx
@@ -0,0 +1,8 @@
+export default function Bulletin() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Bulletin Board</h1>
+      <p>Cleanup events will appear here. (Coming in a week.)</p>
+    </div>
+  )
+}
