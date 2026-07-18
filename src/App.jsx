3b271956e7ef2deb52250ba25c35e32ec267d8d6
Charith Vangavati <cvangavati@gmail.com>
2026-07-17 20:09:41 -0400

    Add NavBar + Cleanup App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,26 @@ import ForgotPassword from './pages/ForgotPassword'
 import ResetPassword from './pages/ResetPassword'
 import ProtectedRoute from './components/ProtectedRoute'
 
-
-function Home() {
-  const { user, signOut } = useAuth()
+function AppLayout() {
   return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h1>🌊 CleanBeach</h1>
-      <p>Logged in as: {user?.email}</p>
-      <button onClick={signOut}>Log Out</button>
-    </div>
-  )
-}
-
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/" element={
           <ProtectedRoute><Bulletin /></ProtectedRoute>
         } />
         <Route path="/events/:id" element={
           <ProtectedRoute><EventDetail /></ProtectedRoute>
         } />
-      <Route path="/profile" element={
+        <Route path="/profile" element={
           <ProtectedRoute><Profile /></ProtectedRoute>
-        } 
-      />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        } />
+      </Routes>
+    </>
   )
 }
 
@@ -55,7 +37,7 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <AppRoutes />
+        <AppLayout />
       </AuthProvider>
     </BrowserRouter>
   )
